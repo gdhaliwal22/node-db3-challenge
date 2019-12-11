5d82a05d2e60e2c1cb5bd9f2a2cb8da36375eff6
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2019-12-11 15:09:37 -0800

    MVP completed

---
+++ schemes/scheme-model.js
@@ -0,0 +1,51 @@
+const db = require("../data/db-config.js");
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
+
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes")
+    .where("id", id)
+    .first();
+}
+
+function findSteps(id) {
+  return db("steps as st")
+    .join("schemes as sc", "st.scheme_id", "sc.id")
+    .select("st.id", "sc.scheme_name", "st.step_number", "st.instructions")
+    .where({ scheme_id: id });
+}
+
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme, "id")
+    .then(([id]) => {
+      return findById(id);
+    });
+}
+
+//
+function update(changes, id) {
+  return db("schemes")
+    .update(changes)
+    .where("id", id)
+    .then(() => {
+      return findById(id);
+    });
+}
+
+function remove(id) {
+  return db("schemes")
+    .where({ id })
+    .delete();
+}
